cb2fe06474f92f05f4f7db3584ee2b116457ce63
ajaysainiimg <104185399+ajaysainiimg@users.noreply.github.com>
2023-02-16 17:20:39 +0530

    changes by ajay
    blogs page , contact us page, categories page

--- assets/js/index.js
+++ assets/js/index.js
@@ -47,91 +47,95 @@ if ($("[page-name=projects]").length) {
     });
 }
 
+if ($("[page-name=mainLandingPage]").length) {
+    $('.example').owlCarousel({
+        loop: true,
+        items: 3,
+        padding: 0,
+        margin: 0,
+        autoplay: true,
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        freeDrag: false,
+        lazyLoad: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true,
+        smartSpeed: 1200,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: false,
+                dots: false
+            },
+            500: {
+                items: 1,
+                nav: false,
+                dots: false
+            },
+            600: {
+                items: 1,
+                nav: false,
+                dots: true
+            },
+            992: {
+                items: 1,
+                nav: false,
+                dots: true
+            },
+            1299: {
+                items: 1,
+                nav: false,
+                dots: true
+            },
+            1499: {
+                items: 1,
+                nav: false,
+                dots: true
+            }
+        },
+    });
 
 
-$('.example').owlCarousel({
-    loop: true,
-    items: 3,
-    padding: 0,
-    margin: 0,
-    autoplay: true,
-    animateOut: 'fadeOut',
-    animateIn: 'fadeIn',
-    freeDrag: false,
-    lazyLoad: true,
-    autoplayTimeout: 2000,
-    autoplayHoverPause: true,
-    smartSpeed: 1200,
-    responsiveClass: true,
-    responsive: {
-        0: {
-            items: 1,
-            nav: false,
-            dots: false
-        },
-        500: {
-            items: 1,
-            nav: false,
-            dots: false
-        },
-        600: {
-            items: 1,
-            nav: false,
-            dots: true
-        },
-        992: {
-            items: 1,
-            nav: false,
-            dots: true
-        },
-        1299: {
-            items: 1,
-            nav: false,
-            dots: true
-        },
-        1499: {
-            items: 1,
-            nav: false,
-            dots: true
-        }
-    },
-});
+    if ($("[page-name=aboutusPage],[page-name=servicesPage]").length) {
+        $('.owlCard').owlCarousel({
+            loop: true,
+            items: 6,
+            padding: 0,
+            margin: 20,
+            nav: true,
+            dots: false,
+            // autoplay: true,
+            // freeDrag: false,
+            // lazyLoad: true,
+            // autoplayTimeout: 2000,
+            // autoplayHoverPause: true,
+            // smartSpeed: 1200,
+            navigation: true,
+            responsiveClass: true,
+            navText: ["<img src='assets/img/icons/owlleftarrow.svg'>","<img src='assets/img/icons/owlrightarrow.svg'>"],
+            responsive: {
+                0: {
+                    items: 1
+                },
+                576: {
+                    items: 1
+                },
+                768: {
+                    items: 2
+                },
+                992: {
+                    items: 3
+                },
+                1299: {
+                    items: 3
+                }
+            },
+        });
+    }
 
-
-$('.owlCard').owlCarousel({
-    loop: true,
-    items: 6,
-    padding: 0,
-    margin: 20,
-    nav: true,
-    dots: false,
-    // autoplay: true,
-    // freeDrag: false,
-    // lazyLoad: true,
-    // autoplayTimeout: 2000,
-    // autoplayHoverPause: true,
-    // smartSpeed: 1200,
-    navigation: true,
-    responsiveClass: true,
-    navText: ["<img src='assets/img/icons/owlleftarrow.svg'>","<img src='assets/img/icons/owlrightarrow.svg'>"],
-    responsive: {
-        0: {
-            items: 1
-        },
-        576: {
-            items: 1
-        },
-        768: {
-            items: 2
-        },
-        992: {
-            items: 3
-        },
-        1299: {
-            items: 3
-        }
-    },
-});
+    
+}
 
 
 (() => {
